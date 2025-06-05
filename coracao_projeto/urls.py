89cec0ab6956a87eb0de71da4b340c5c4fc94273
Icaro Vieira <icarosug@gmail.com>
2025-06-05 10:33:57 -0300

    Config pasta .vscode

--- coracao_projeto/urls.py
+++ coracao_projeto/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for config project.
+URL configuration for coracao_projeto project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.2/topics/http/urls/
@@ -16,11 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from . import include
-
-# Define as rotas no projeto
 
 urlpatterns = [
-    path('Home', admin.site.urls),
-    path('', include('core.urls'))# delega rotas para o app
+    path('admin/', admin.site.urls),
 ]
